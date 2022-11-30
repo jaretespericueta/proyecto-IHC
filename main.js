7e823882b3947b794b44f2def2e038894991cdf2
jaretespericueta <112997640+jaretespericueta@users.noreply.github.com>
2022-11-29 19:25:02 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const fila = document.querySelector('.contenedor-carousel');
+const peliculas = document.querySelector ('.pelicula');
+
+const flechaIzquierda = document.getElementById('flecha-izquierda');
+const flechaDerecha = document.getElementById('flecha-derecha');
+
+flechaDerecha.addEventListener('click', () => {
+    fila.scrollLeft += fila.offsetWidth;
+});
+
+flechaIzquierda.addEventListener('click', () => {
+    fila.scrollLeft -= fila.offsetWidth;
+});
